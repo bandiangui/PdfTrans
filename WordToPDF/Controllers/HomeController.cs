8971f42150a8ac63ddc0d3d94d6cddca1a6a0825
bandiangui <bandiangui001@126.com>
2014-04-17 00:43:48 +0800

    word->pdf->image->pdf水印

--- WordToPDF/Controllers/HomeController.cs
+++ WordToPDF/Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Aspose.Pdf.Generator;
 using WordToPDF.Models;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
 using System.IO;
-//using Aspose.Words;
-//using Aspose.Pdf;
+//using Aspose.Pdf.Generator;
+using Aspose.Words;
+using Aspose.Pdf;
+using Aspose.Pdf.Devices;
+using Aspose.Pdf.Generator;
 
 namespace WordToPDF.Controllers
 {
@@ -19,27 +21,65 @@ namespace WordToPDF.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-            //Document doc = new Document(@"E:\工作文档\模板属性.docx");
-            //doc.Save(@"E:\工作文档\1\" + Guid.NewGuid().ToString() + ".pdf", SaveFormat.Pdf);
+
+
+            Aspose.Words.Document doc = new Aspose.Words.Document(@"C:\Users\芸芸\Desktop\2013年工作总结.doc");
 
             Pdf pdf = new Pdf();
             Aspose.Pdf.Generator.Section section = pdf.Sections.Add();
 
-            System.Drawing.Image imge = System.Drawing.Image.FromFile(@"C:\Users\芸芸\Desktop\cb71b07fc15cbcc3e587e1e83bc751c7.jpg");
-            AddWaterMark(ref imge);
+            using (FileStream fs = new FileStream(@"E:\2\" + Guid.NewGuid().ToString() + "___1.pdf", FileMode.Create))
+            {
+                doc.Save(fs, Aspose.Words.SaveFormat.Pdf);
+
+                // 把pdf转换为图片,并加水印
+                Aspose.Pdf.Document pdfDoc = new Aspose.Pdf.Document(fs);
+                var docPageInfo = doc.GetPageInfo(1);
+                section.PageInfo.PageWidth = docPageInfo.WidthInPoints;
+                section.PageInfo.PageHeight = docPageInfo.HeightInPoints;
+                section.PageInfo.Margin = new Aspose.Pdf.Generator.MarginInfo();
+
+                int imgWith = (int)docPageInfo.WidthInPoints / 72 * 300;
+                int imgHeight = (int)docPageInfo.HeightInPoints / 72 * 300;
+
+                for (int pageCount = 1; pageCount <= pdfDoc.Pages.Count; pageCount++)
+                {
+                    Resolution resolution = new Resolution(300);
+
+                    JpegDevice jpgBuilder = new JpegDevice(imgWith, imgHeight, resolution, 100);
+                    using (MemoryStream jpgMs = new MemoryStream())
+                    {
+                        jpgBuilder.Process(pdfDoc.Pages[pageCount], jpgMs);
+
+                        System.Drawing.Image img = System.Drawing.Image.FromStream(jpgMs);
+                        ImgAddWaterMark(ref img);
+
+                        Aspose.Pdf.Generator.Image pdfImg = Aspose.Pdf.Generator.Image.FromSystemImage(img);
+
+                        section.Paragraphs.Add(pdfImg);
+                    }
+
+                }
+            }
+            pdf.Save(@"E:\2\" + Guid.NewGuid().ToString() + ".pdf");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+            // 从图片到PDF
+            //Pdf pdf = new Pdf();
+            //Aspose.Pdf.Generator.Section section = pdf.Sections.Add();
+
+            //System.Drawing.Image imge = System.Drawing.Image.FromFile(@"C:\Users\芸芸\Desktop\cb71b07fc15cbcc3e587e1e83bc751c7.jpg");
+            //ImgAddWaterMark(ref imge);
 
             //Aspose.Pdf.Generator.Image img = Aspose.Pdf.Generator.Image.FromSystemImage(imge);
             //img.ImageInfo.ImageFileType = ImageFileType.Jpeg;
             //section.PageInfo.PageWidth = imge.Width / imge.HorizontalResolution * 72;
             //section.PageInfo.PageHeight = imge.Height / imge.HorizontalResolution * 72;
             //section.PageInfo.Margin = new MarginInfo();
-
-            //FloatingBox fb = new FloatingBox(700, 800);
-            //TextInfo ti = new TextInfo();
-            //ti.Color = new Color("#0000ff");
-            //ti.FontSize = 24;
-            //ti.IsTrueTypeFontBold = true;
-            //fb.Paragraphs.Add(new Text("这里是水印", ti));
 
             //pdf.Watermarks.Add(fb);
 
@@ -52,13 +92,6 @@ namespace WordToPDF.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -66,7 +99,7 @@ namespace WordToPDF.Controllers
             return View();
         }
 
-        public void AddWaterMark(ref System.Drawing.Image image)
+        public void ImgAddWaterMark(ref System.Drawing.Image image)
         {
             System.Drawing.Graphics graph = System.Drawing.Graphics.FromImage(image);
             graph.DrawImage(image, 0, 0, image.Width, image.Height);
@@ -77,14 +110,13 @@ namespace WordToPDF.Controllers
 
             using (MemoryStream ms = new MemoryStream())
             {
-                Watermark.WaterImage wt = new Watermark.WaterImage(image.Width, image.Height);
+                WordToPDF.Models.Watermark.WaterImage wt = new WordToPDF.Models.Watermark.WaterImage(image.Width, image.Height);
+                wt.FontSize = 200;
                 wt.Create();
                 graph.DrawImage(wt.ResultImage, new System.Drawing.Rectangle(0, 0, wt.ResultImage.Width, wt.ResultImage.Height)
                     , 0, 0, wt.ResultImage.Width, wt.ResultImage.Height, GraphicsUnit.Pixel, wt.SetTransparency(wt.Transparency));
             }
 
-            image.Save(@"E:\1\" + Guid.NewGuid().ToString() + ".jpg", ImageFormat.Jpeg);
-            image.Dispose();
             graph.Dispose();
         }
     }
